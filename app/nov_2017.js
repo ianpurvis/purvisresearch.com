7c7e7418f6a87c8a4b09720c1b24f113651fc33a
Ian Purvis <ian.c.purvis@gmail.com>
2017-11-30 11:47:48 -0500

    Wires up basic three graphics

--- app/nov_2017.js
+++ app/nov_2017.js
@@ -1,3 +1,7 @@
+var THREE = require('three')
+
+var renderer, scene, camera
+
 document.addEventListener('DOMContentLoaded', startGraphics)
 window.addEventListener('resize', maximizeGraphics)
 
@@ -13,12 +17,44 @@ function fullSize() {
 function maximizeGraphics() {
   let {width, height} = fullSize()
   console.debug(`PR: Resizing graphics to ${width}x${height}`)
+
+  renderer.setSize(width, height)
+  camera.aspect = width / height
+  camera.updateProjectionMatrix()
 }
 
 
 function startGraphics() {
+  console.debug("PR: Starting graphics...")
+
   let {width, height} = fullSize()
 
-  console.debug("PR: Starting graphics...")
+  renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: false
+  })
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(width, height)
+  document.body.appendChild(renderer.domElement)
+
+  // Create scene
+  scene = new THREE.Scene()
+
+  camera = new THREE.PerspectiveCamera(60, width / height, 1, 10000)
+  camera.position.z = 100
+  camera.lookAt(scene.position)
+
+  // Add content
+  let geometry = new THREE.SphereBufferGeometry(5, 32, 32)
+  let material = new THREE.MeshBasicMaterial({color: 0xffff00})
+  let mesh = new THREE.Mesh(geometry, material)
+  scene.add(mesh)
+
+  animate()
 }
 
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+}
