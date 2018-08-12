7888a869aa6c00c977ab8d810845a490897c58d0
Ian Purvis <ian.c.purvis@gmail.com>
2018-08-12 20:54:44 +0900

    Adds material overrides

--- assets/javascripts/aug_2018.js
+++ assets/javascripts/aug_2018.js
@@ -48,7 +48,22 @@ export class Demo {
       new GLTFLoader().load(
         '/models/basket/asset.gltf',
         (gltf) => {
-          self.scene.add(gltf.scene)
+          let basket = gltf.scene.children[0]
+          basket.geometry.computeFaceNormals()
+          basket.geometry.computeVertexNormals()
+          //basket.material = new THREE.MeshNormalMaterial({
+            //wireframe: true,
+          //})
+          basket.material = new THREE.MeshBasicMaterial({
+            color: 0xff00ff,
+            wireframe: true,
+          })
+          self.scene.add(basket)
+
+          // If using basket texture, supply light:
+          //let light = new THREE.AmbientLight(0xffffff)
+          //this.scene.add(light)
+
           resolve(self)
         },
         (xhr) => {
