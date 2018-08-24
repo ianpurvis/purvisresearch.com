a28c2ce8dbbf4fa6d4250439ac9fecb48c5f0af4
Ian Purvis <ian.c.purvis@gmail.com>
2018-08-24 16:32:42 +0900

    Refactors aug 2018 demo internals

--- assets/javascripts/aug_2018.js
+++ assets/javascripts/aug_2018.js
@@ -7,7 +7,8 @@ import Random from '~/assets/javascripts/random.js'
 
 export default class Aug2018Demo extends ThreeDemo {
 
-  layoutScene(basket) {
+  layoutScene(gltf) {
+    let basket = gltf.scene.children[0]
     basket.material = this.randomMaterial({color: 0xff00ff})
     basket.geometry.center()
     this.scene.add(basket)
@@ -17,14 +18,20 @@ export default class Aug2018Demo extends ThreeDemo {
     clone.position.x += Random.rand({min: -10, max: 10})
     this.scene.add(clone)
 
-    this.randomizePointOfView({subject: basket})
+    this.camera.position.copy(this.randomPointOfView({subject: basket}))
+    this.camera.lookAt(basket.position)
   }
 
+
   load() {
-    return super.load()
-      .then(this.loadBasket.bind(this))
+    return import('~/assets/models/basket.draco.glb')
+      .then(this.parseGLTF.bind(this))
       .then(this.layoutScene.bind(this))
+      .catch((error) => {
+        console.error(`An error happened ${error}`)
+      })
   }
+
 
   loadDracoDecoder() {
     return new Promise((resolve) => {
@@ -38,25 +45,13 @@ export default class Aug2018Demo extends ThreeDemo {
     })
   }
 
-  loadBasket() {
+
+  parseGLTF(buffer) {
     THREE.DRACOLoader.decoderModulePromise = this.loadDracoDecoder()
-    let loader = new GLTFLoader()
-    loader.setDRACOLoader(new THREE.DRACOLoader())
     return new Promise((resolve, reject) => {
-      return import('~/assets/models/basket.draco.glb').then((buffer) => {
-        return loader.parse(
-          buffer,
-          '/',
-          (gltf) => {
-            let basket = gltf.scene.children[0]
-            resolve(basket)
-          },
-          (exception) => {
-            console.error(`An error happened ${exception}`)
-            reject(exception)
-          }
-        )
-      })
+      let loader = new GLTFLoader()
+      loader.setDRACOLoader(new THREE.DRACOLoader())
+      loader.parse(buffer, '/', resolve, reject)
     })
   }
 
@@ -76,7 +71,7 @@ export default class Aug2018Demo extends ThreeDemo {
   }
 
 
-  randomizePointOfView({subject}) {
+  randomPointOfView({subject}) {
     if (!subject.geometry.boundingSphere) {
       subject.geometry.computeBoundingSphere()
     }
@@ -85,8 +80,9 @@ export default class Aug2018Demo extends ThreeDemo {
       Random.rand({min: -360, max: 360}),
       Random.rand({min: -360, max: 360})
     )
-    this.camera.position.setFromSpherical(sphere)
-    this.camera.lookAt(subject.position)
+    let position = new THREE.Vector3()
+    position.setFromSpherical(sphere)
+    return position
   }
 
 
