9d6ae8d41dbe3642cd3c8652d15b2c7c6ff1c3a7
Ian Purvis <ian.c.purvis@gmail.com>
2018-09-10 18:39:07 +0900

    Adds description, tweaks title

--- pages/2018/sept.js
+++ pages/2018/sept.js
@@ -12,18 +12,18 @@ export default {
     return {
       animationFrame: null,
       demo: null,
-      title: "september 2018 - purvis research"
+      title: "sept 2018 - purvis research"
     }
   },
   head() {
     return {
       title: this.title,
       meta: [
-        { property:"og:description", content:"" },
+        { property:"og:description", content:"A scanned 3d model, randomly rendered in WebGL" },
         { property:"og:image", content:"http://purvisresearch.com/images/2018/sept.png" },
         { property:"og:image:height", content:"619" },
         { property:"og:image:width", content:"1183" },
-        { property:"og:title", content:"September 2018" },
+        { property:"og:title", content:"Sept 2018" },
         { name:"twitter:card", content:"summary_large_image" },
       ]
     }
