be4efd35ee947224a99fc26bb3a5636bde5e8569
Ian Purvis <ian.c.purvis@gmail.com>
2018-12-12 16:47:30 +0900

    Improves texture loader error handling

--- assets/javascripts/texture_loader.js
+++ assets/javascripts/texture_loader.js
@@ -4,7 +4,9 @@ class TextureLoader extends THREETextureLoader {
 
   load(url, onProgress = () => {}) {
     return new Promise((resolve, reject) => {
-      super.load(url, resolve, onProgress, reject)
+      super.load(url, resolve, onProgress, (event) => {
+        reject(new Error('TextureLoader: Could not load url.'))
+      })
     })
   }
 }
