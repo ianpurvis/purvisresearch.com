ad7c4cf5eb9e84b79284062bdee500099b8b4e44
Ian Purvis <ian.c.purvis@gmail.com>
2019-04-11 11:33:10 -0400

    Adds digital art experiment links and logo

--- pages/index.vue
+++ pages/index.vue
@@ -25,9 +25,18 @@
       </h2>
     </div>
     <div class="boxx">
+      <div class="snake">DIGITAL_ART_EXPERIMENTS_2019_APR_<nuxt-link to="2019/apr.html">SURREAL_TELEVISION_WITH_WEBRTC_AND_WEBGL</nuxt-link>_2018_OCT_<nuxt-link to="2018/oct.html">SCREEN_PRINTING_A_3D_SCAN_WITH_WEBGL</nuxt-link>_2017_NOV_<nuxt-link to="2017/nov.html">A_3D_CHARACTER_EXPLODER_IN_WEBGL</nuxt-link>_2017_OCT_<nuxt-link to="2017/oct.html">A_BÉZIER_MOIRÉ_GENERATOR_IN_WEBGL</nuxt-link>_2017_SEP_<nuxt-link to="2017/sept.html">AN_EMOJI_PARTICLE_FLOW_IN_WEBGL</nuxt-link></div>
+    </div>
+    <div class="boxx">
+      <nuxt-link to="/">
+        <figure class="image">
+          <img src="~/assets/images/logo.png"/>
+        </figure>
+      </nuxt-link>
+    </div>
+    <div class="boxx">
       <obfuscated-mailto></obfuscated-mailto>
-      <br/>
-      <nuxt-link to="/">https://purvisresearch.com</nuxt-link>
+      <a href="https://github.com/ianpurvis">github.com/ianpurvis</a>
     </div>
   </main>
 </template>
