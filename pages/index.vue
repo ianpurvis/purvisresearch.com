22ca6b1d171a062c9e7d7a17db1f44587bf9b2b2
Ian Purvis <ian.c.purvis@gmail.com>
2019-04-12 16:57:13 -0400

    Moves logo to top of page

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,13 @@
   <main>
     <autoscaled-div :max-scale=2.8>
       <div class="boxx">
+        <nuxt-link to="/">
+          <figure class="image">
+            <img src="~/assets/images/logo.png"/>
+          </figure>
+        </nuxt-link>
+      </div>
+      <div class="boxx">
         <h1>IAN PURVIS</h1>
         <h2 class="snake">STARTUP_TECHNOLOGY_RESEARCH_DESIGN_DEVELOPMENT_TESTING_DEVOPS_PROJECT_MANAGEMENT</h2>
       </div>
@@ -27,13 +34,6 @@
       </div>
       <div class="boxx">
         <div class="snake experiments">DIGITAL_ART_EXPERIMENTS_2019_APR_<nuxt-link to="2019/apr.html">SURREAL_WEBRTC_TELEVISION</nuxt-link>_2018_OCT_<nuxt-link to="2018/oct.html">SCREEN_PRINTING_A_3D_SCAN</nuxt-link>_2017_NOV_<nuxt-link to="2017/nov.html">A_3D_CHARACTER_EXPLODER</nuxt-link>_2017_OCT_<nuxt-link to="2017/oct.html">A_BÉZIER_MOIRÉ_GENERATOR</nuxt-link>_2017_SEP_<nuxt-link to="2017/sept.html">AN_EMOJI_PARTICLE_FLOW</nuxt-link></div>
-      </div>
-      <div class="boxx">
-        <nuxt-link to="/">
-          <figure class="image">
-            <img src="~/assets/images/logo.png"/>
-          </figure>
-        </nuxt-link>
       </div>
       <div class="boxx">
         <obfuscated-mailto></obfuscated-mailto>
