0ee45c4ace083ab99d3b231a2ff7ff7200014748
Ian Purvis <ian.c.purvis@gmail.com>
2019-04-12 17:36:53 -0400

    Adds call to action an kaomoji

--- pages/index.vue
+++ pages/index.vue
@@ -36,8 +36,12 @@
         <div class="snake experiments">DIGITAL_ART_EXPERIMENTS_2019_APR_<nuxt-link to="2019/apr.html">SURREAL_WEBRTC_TELEVISION</nuxt-link>_2018_OCT_<nuxt-link to="2018/oct.html">SCREEN_PRINTING_A_3D_SCAN</nuxt-link>_2017_NOV_<nuxt-link to="2017/nov.html">A_3D_CHARACTER_EXPLODER</nuxt-link>_2017_OCT_<nuxt-link to="2017/oct.html">A_BÉZIER_MOIRÉ_GENERATOR</nuxt-link>_2017_SEP_<nuxt-link to="2017/sept.html">AN_EMOJI_PARTICLE_FLOW</nuxt-link></div>
       </div>
       <div class="boxx">
+        <p class="snake">I love working with startups, <em>creative</em> people, and <em>interesting</em> tech. Hit me up!</p>
         <obfuscated-mailto></obfuscated-mailto>
         <a href="https://github.com/ianpurvis">github.com/ianpurvis</a>
+      </div>
+      <div class="boxx">
+        <div class="kaomoji is-pulled-right">ヾ(⌐■_■)ノ</div>
       </div>
     </autoscaled-div>
   </main>
