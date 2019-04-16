98e7e23805279d047cd5cb796726d0121f5462b2
Ian Purvis <ian.c.purvis@gmail.com>
2019-04-16 11:51:56 -0400

    Adds og preview image for top index

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default {
       meta: [
         { name: 'description', content: this.description, hid: 'description' },
         { property:"og:description", content: this.description },
-        { property:"og:image", content: `${organization.url}${require("~/assets/images/2019/apr.png")}` },
+        { property:"og:image", content: `${organization.url}${require("~/assets/images/index.png")}` },
         { property:"og:image:height", content:"859" },
         { property:"og:image:width", content:"1646" },
         { property:"og:title", content: this.title },
