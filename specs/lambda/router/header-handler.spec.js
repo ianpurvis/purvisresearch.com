17f14c95cd09a3c0623934cd861d1775e531b941
Ian Purvis <ian.c.purvis@gmail.com>
2019-06-02 14:45:34 -0400

    Adds spec coverage

--- specs/lambda/router/header-handler.spec.js
+++ specs/lambda/router/header-handler.spec.js
@@ -1,110 +1,151 @@
 import { handler } from '~/lambda/router/header-handler.js'
-import { event } from '~/specs/fixtures'
 
 describe('handler', () => {
+  describe('given a request', () => {
+    const given = {
+      Records: [{
+        cf: {
+          request: {
+            method: 'GET',
+            uri: '/'
+          },
+          response: {
+            status: '200'
+          }
+        }
+      }]
+    }
+    it('returns a response with status', async () => {
+      const response = await handler(given)
+      expect(response).toMatchObject({
+        status: '200'
+      })
+    })
+    it('returns a response with Cache-Control', async () => {
+      const response = await handler(given)
+      expect(response).toMatchObject({
+        headers: {
+          "cache-control": [
+            {
+              "key": "Cache-Control",
+              "value": "public, max-age=0",
+            },
+          ],
+        }
+      })
+    })
 
-  it('returns a response with status', async () => {
-    const response = await handler(event)
-    expect(response).toMatchObject({
-      status: '200'
+    it('returns a response with Content-Security-Policy', async () => {
+      const response = await handler(given)
+      expect(response).toMatchObject({
+        headers: {
+          "content-security-policy": [
+            {
+              "key": "Content-Security-Policy",
+              "value": "base-uri 'none'; connect-src 'self' https://www.google-analytics.com; default-src 'none'; frame-ancestors 'none'; font-src 'self' data:; form-action 'none'; img-src 'self' https://www.google-analytics.com; manifest-src 'self'; media-src 'self'; object-src 'none'; script-src 'self' 'sha256-V/WaLGhSS+tTPAMDVjFgErm2VGPm+tNBC1rdDJHVkZ0=' https://www.google-analytics.com; style-src 'self' 'unsafe-inline'",
+            },
+          ],
+        }
+      })
+    })
+
+    it('returns a response with Referrer-Policy', async () => {
+      const response = await handler(given)
+      expect(response).toMatchObject({
+        headers: {
+          "referrer-policy": [
+            {
+              "key": "Referrer-Policy",
+              "value": "no-referrer-when-downgrade",
+            },
+          ],
+        }
+      })
+    })
+
+    it('returns a response with Strict-Transport-Policy', async () => {
+      const response = await handler(given)
+      expect(response).toMatchObject({
+        headers: {
+          "strict-transport-security": [
+            {
+              "key": "Strict-Transport-Security",
+              "value": "max-age=31556952; includeSubdomains; preload",
+            },
+          ],
+        }
+      })
+    })
+
+    it('returns a response with X-Content-Type-Options', async () => {
+      const response = await handler(given)
+      expect(response).toMatchObject({
+        headers: {
+          "x-content-type-options": [
+            {
+              "key": "X-Content-Type-Options",
+              "value": "nosniff",
+            },
+          ],
+        }
+      })
+    })
+
+    it('returns a response with X-Frame-Options', async () => {
+      const response = await handler(given)
+      expect(response).toMatchObject({
+        headers: {
+          "x-frame-options": [
+            {
+              "key": "X-Frame-Options",
+              "value": "DENY",
+            },
+          ],
+        }
+      })
+    })
+
+    it('returns a response with X-XSS-Protection', async () => {
+      const response = await handler(given)
+      expect(response).toMatchObject({
+        headers: {
+          "x-xss-protection": [
+            {
+              "key": "X-XSS-Protection",
+              "value": "1; mode=block",
+            },
+          ],
+        }
+      })
     })
   })
 
-  it('returns a response with Cache-Control', async () => {
-    const response = await handler(event)
-    expect(response).toMatchObject({
-      headers: {
-        "cache-control": [
-          {
-            "key": "Cache-Control",
-            "value": "public, max-age=0",
+  describe('given an asset request', () => {
+    const given = {
+      Records: [{
+        cf: {
+          request: {
+            method: 'GET',
+            uri: '/_/images/example.png'
           },
-        ],
-      }
-    })
-  })
-
-  it('returns a response with Content-Security-Policy', async () => {
-    const response = await handler(event)
-    expect(response).toMatchObject({
-      headers: {
-        "content-security-policy": [
-          {
-            "key": "Content-Security-Policy",
-            "value": "base-uri 'none'; connect-src 'self' https://www.google-analytics.com; default-src 'none'; frame-ancestors 'none'; font-src 'self' data:; form-action 'none'; img-src 'self' https://www.google-analytics.com; manifest-src 'self'; media-src 'self'; object-src 'none'; script-src 'self' 'sha256-V/WaLGhSS+tTPAMDVjFgErm2VGPm+tNBC1rdDJHVkZ0=' https://www.google-analytics.com; style-src 'self' 'unsafe-inline'",
-          },
-        ],
-      }
-    })
-  })
-
-  it('returns a response with Referrer-Policy', async () => {
-    const response = await handler(event)
-    expect(response).toMatchObject({
-      headers: {
-        "referrer-policy": [
-          {
-            "key": "Referrer-Policy",
-            "value": "no-referrer-when-downgrade",
-          },
-        ],
-      }
-    })
-  })
-
-  it('returns a response with Strict-Transport-Policy', async () => {
-    const response = await handler(event)
-    expect(response).toMatchObject({
-      headers: {
-        "strict-transport-security": [
-          {
-            "key": "Strict-Transport-Security",
-            "value": "max-age=31556952; includeSubdomains; preload",
-          },
-        ],
-      }
-    })
-  })
-
-  it('returns a response with X-Content-Type-Options', async () => {
-    const response = await handler(event)
-    expect(response).toMatchObject({
-      headers: {
-        "x-content-type-options": [
-          {
-            "key": "X-Content-Type-Options",
-            "value": "nosniff",
-          },
-        ],
-      }
-    })
-  })
-
-  it('returns a response with X-Frame-Options', async () => {
-    const response = await handler(event)
-    expect(response).toMatchObject({
-      headers: {
-        "x-frame-options": [
-          {
-            "key": "X-Frame-Options",
-            "value": "DENY",
-          },
-        ],
-      }
-    })
-  })
-
-  it('returns a response with X-XSS-Protection', async () => {
-    const response = await handler(event)
-    expect(response).toMatchObject({
-      headers: {
-        "x-xss-protection": [
-          {
-            "key": "X-XSS-Protection",
-            "value": "1; mode=block",
-          },
-        ],
-      }
+          response: {
+            status: '200'
+          }
+        }
+      }]
+    }
+    it('returns a response with Cache-Control', async () => {
+      const response = await handler(given)
+      expect(response).toMatchObject({
+        headers: {
+          "cache-control": [
+            {
+              "key": "Cache-Control",
+              "value": "public, max-age=31556952",
+            },
+          ],
+        }
+      })
     })
   })
 })
--- specs/lambda/router/redirect-handler.spec.js
+++ specs/lambda/router/redirect-handler.spec.js
@@ -1,22 +1,30 @@
 import { handler } from '~/lambda/router/redirect-handler.js'
-import { event, eventThatNeedsRedirection } from '~/specs/fixtures'
 
 describe('handler', () => {
   describe('given a redirectable request', () => {
-    const given = eventThatNeedsRedirection
-
+    const given = {
+      Records: [{
+        cf: {
+          request: {
+            method: 'GET',
+            uri: '/sept_2017.html'
+          },
+          response: {
+            status: '200'
+          }
+        }
+      }]
+    }
     it('returns a response with 301 status', async () => {
       const response = await handler(given)
       expect(response).toMatchObject({
         status: '301'
       })
     })
-
     it('returns a response with empty body', async () => {
       const response = await handler(given)
       expect(response).not.toHaveProperty('body')
     })
-
     it('returns a response with empty body', async () => {
       const response = await handler(given)
       expect(response).toMatchObject({
@@ -28,8 +36,19 @@ describe('handler', () => {
   })
 
   describe('given a non-redirectable request', () => {
-    const given = event
-
+    const given = {
+      Records: [{
+        cf: {
+          request: {
+            method: 'GET',
+            uri: '/'
+          },
+          response: {
+            status: '200'
+          }
+        }
+      }]
+    }
     it('returns the upstream response', async () => {
       const response = await handler(given)
       expect(response).toMatchObject(given.Records[0].cf.response)
