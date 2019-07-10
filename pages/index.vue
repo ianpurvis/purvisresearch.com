fe36deb8aec323d90cb2cfdadbb58c2aad28153a
Ian Purvis <ian.c.purvis@gmail.com>
2019-07-10 12:19:14 -0500

    Migrates experiments list to v-tight

--- pages/index.vue
+++ pages/index.vue
@@ -44,10 +44,13 @@
         </ul>
       </div>
       <div class="boxx">
-        <nav class="experiments">
-          <h2 v-html="snake('Digital Art Experiments')" /><ul class="snake-list">
-            <li v-for="(experiment, index) in experiments" :key="index">
-              <span v-html="snake(experiment.title)"></span><span class="underscore">&nbsp;</span><nuxt-link :to="experiment.route" v-html="snake(experiment.description)"></nuxt-link>
+        <nav class="experiments" v-tight>
+          <h2 v-html="snake('Digital Art Experiments')"></h2>
+          <ul class="snake-list" v-tight>
+            <li v-for="(experiment, index) in experiments" :key="index" v-tight>
+              <span v-html="snake(experiment.title)"></span>
+              <span class="underscore">&nbsp;</span>
+              <nuxt-link :to="experiment.route" v-html="snake(experiment.description)"></nuxt-link>
             </li>
           </ul>
         </nav>
