a926b4e265ae5fb6f8ba2fe97503d90ff615ccae
Ian Purvis <ian.c.purvis@gmail.com>
2019-07-16 12:00:14 -0500

    Refines spec definitions

--- specs/lambda/middleware/content-security-policy.spec.js
+++ specs/lambda/middleware/content-security-policy.spec.js
@@ -2,65 +2,67 @@ import { call, policyForNuxt } from '~/lambda/middleware/content-security-policy
 
 describe('call({ request, response })', () => {
   let request, response, result
-  describe('given a request and an html response', () => {
-    beforeEach(() => {
-      request = Object.freeze({
-        method: 'GET',
-        uri: '/example.html'
-      })
-      response = {
-        status: '200',
-        headers: {
-          'content-type': [{
-            key: 'Content-Type',
-            value: 'text/html; charset=utf-8'
-          }]
-        }
-      }
-    })
-    it('returns the unmodified request', async () => {
-      result = await call({ request, response })
-      expect(result.request).toBe(request)
-    })
-    it('returns the response with a Content-Security-Policy header', async () => {
-      const policy = policyForNuxt()
-      result = await call({ request, response })
-      expect(result.response).toMatchObject(response)
-      expect(result.response).toMatchObject({
-        headers: {
-          "content-security-policy": [
-            {
-              "key": "Content-Security-Policy",
-              "value": policy
-            },
-          ],
+  describe('given a request and a response', () => {
+    describe('when response is html', () => {
+      beforeEach(() => {
+        request = Object.freeze({
+          method: 'GET',
+          uri: '/example.html'
+        })
+        response = {
+          status: '200',
+          headers: {
+            'content-type': [{
+              key: 'Content-Type',
+              value: 'text/html; charset=utf-8'
+            }]
+          }
         }
       })
-    })
-  })
-  describe('given a request and a non-html response', () => {
-    beforeEach(() => {
-      request = Object.freeze({
-        method: 'GET',
-        uri: '/example.png'
+      it('returns the unmodified request', async () => {
+        result = await call({ request, response })
+        expect(result.request).toBe(request)
       })
-      response = Object.freeze({
-        status: '200',
-        headers: {
-          'content-type': [{
-            key: 'Content-Type',
-            value: 'image/png'
-          }]
-        }
+      it('returns the response with a Content-Security-Policy header', async () => {
+        const policy = policyForNuxt()
+        result = await call({ request, response })
+        expect(result.response).toMatchObject(response)
+        expect(result.response).toMatchObject({
+          headers: {
+            "content-security-policy": [
+              {
+                "key": "Content-Security-Policy",
+                "value": policy
+              },
+            ],
+          }
+        })
       })
     })
-    it('returns the unmodified request', async () => {
-      result = await call({ request, response })
-      expect(result.request).toBe(request)
-    })
-    it('returns the unmodified response', async () => {
-      result = await call({ request, response })
-      expect(result.response).toBe(response)
+    describe('when response is not html', () => {
+      beforeEach(() => {
+        request = Object.freeze({
+          method: 'GET',
+          uri: '/example.png'
+        })
+        response = Object.freeze({
+          status: '200',
+          headers: {
+            'content-type': [{
+              key: 'Content-Type',
+              value: 'image/png'
+            }]
+          }
+        })
+      })
+      it('returns the unmodified request', async () => {
+        result = await call({ request, response })
+        expect(result.request).toBe(request)
+      })
+      it('returns the unmodified response', async () => {
+        result = await call({ request, response })
+        expect(result.response).toBe(response)
+      })
     })
   })
   describe('given a request only', () => {
--- specs/lambda/middleware/redirection.spec.js
+++ specs/lambda/middleware/redirection.spec.js
@@ -3,49 +3,50 @@ import { call } from '~/lambda/middleware/redirection.js'
 describe('call({ request, response })', () => {
   let request, response, result
 
-  describe('given a redirectable request and no response', () => {
-    beforeEach(() => {
-      request = Object.freeze({
-        method: 'GET',
-        uri: '/sept_2017.html'
+  describe('given a request only', () => {
+    describe('when request is redirectable', () => {
+      beforeEach(() => {
+        request = Object.freeze({
+          method: 'GET',
+          uri: '/sept_2017.html'
+        })
+        response = undefined
       })
-      response = undefined
-    })
-    it('returns the unmodified request', async () => {
-      result = await call({ request, response })
-      expect(result.request).toBe(request)
-    })
-    it('returns a redirect response', async () => {
-      result = await call({ request, response })
-      expect(result.response).toStrictEqual({
-        status: '301',
-        statusDescription: 'Moved Permanently',
-        headers: {
-          'location': [{
-            key: 'Location',
-            value: '/2017/sept.html'
-          }]
-        }
-        // Note, no body
+      it('returns the unmodified request', async () => {
+        result = await call({ request, response })
+        expect(result.request).toBe(request)
+      })
+      it('returns a redirect response', async () => {
+        result = await call({ request, response })
+        expect(result.response).toStrictEqual({
+          status: '301',
+          statusDescription: 'Moved Permanently',
+          headers: {
+            'location': [{
+              key: 'Location',
+              value: '/2017/sept.html'
+            }]
+          }
+          // Note, no body
+        })
       })
     })
-  })
-
-  describe('given a non-redirectable request only', () => {
-    beforeEach(() => {
-      request = Object.freeze({
-        method: 'GET',
-        uri: '/'
+    describe('when request is not redirectable', () => {
+      beforeEach(() => {
+        request = Object.freeze({
+          method: 'GET',
+          uri: '/'
+        })
+        response = undefined
       })
-      response = undefined
-    })
-    it('returns the unmodified request', async () => {
-      result = await call({ request, response })
-      expect(result.request).toBe(request)
-    })
-    it('returns the unmodified response', async () => {
-      result = await call({ request, response })
-      expect(result.response).toBe(response)
+      it('returns the unmodified request', async () => {
+        result = await call({ request, response })
+        expect(result.request).toBe(request)
+      })
+      it('returns the unmodified response', async () => {
+        result = await call({ request, response })
+        expect(result.response).toBe(response)
+      })
     })
   })
 })
