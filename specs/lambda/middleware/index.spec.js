dc5ebe2e2fa2f63df2c6e8f14793e8dece225921
Ian Purvis <ian.c.purvis@gmail.com>
2019-07-16 15:47:12 -0500

    Cleans up spec fixtures, improves spec for middleware handler

--- specs/lambda/middleware/index.spec.js
+++ specs/lambda/middleware/index.spec.js
@@ -1,22 +1,68 @@
-import { handler } from '~/lambda/middleware/index.js'
-import { event, eventThatNeedsRedirection } from '~/specs/fixtures'
+jest.mock('~/lambda/middleware/cache-control.js')
+jest.mock('~/lambda/middleware/content-security-policy.js')
+jest.mock('~/lambda/middleware/headers.js')
+jest.mock('~/lambda/middleware/redirection.js')
 
-describe('handler', () => {
-  describe('given an event', () => {
-    const given = event
-    it('returns a response with status', async () => {
-      const response = await handler(given)
-      expect(response).toMatchObject({
-        status: '200'
+import cacheControl from '~/lambda/middleware/cache-control.js'
+import contentSecurityPolicy from '~/lambda/middleware/content-security-policy.js'
+import headers from '~/lambda/middleware/headers.js'
+import redirection from '~/lambda/middleware/redirection.js'
+import { handler } from '~/lambda/middleware/index.js'
+import { originRequestEvent } from '~/specs/fixtures'
+
+describe('handler(event)', () => {
+  let event, mocks, result
+
+  describe('given a cloudfront origin request event', () => {
+    beforeEach(() => {
+      event = originRequestEvent
+      mocks = {
+        request: {
+          example: 0
+        },
+        response: {
+          example: 0
+        }
+      }
+
+      ;[
+        redirection,
+        headers,
+        cacheControl,
+        contentSecurityPolicy,
+      ].forEach(middleware => {
+        middleware.call.mockResolvedValue(mocks)
       })
     })
-  })
-  describe('given an event that needs redirection', () => {
-    const given = eventThatNeedsRedirection
-    it('returns a response with status', async () => {
-      const response = await handler(given)
-      expect(response).toMatchObject({
-        status: '301'
+
+    it('calls each middleware', async () => {
+      await handler(event)
+      const { request, response } = event.Records[0].cf
+      expect(redirection.call).toHaveBeenCalledWith({ request, response })
+      expect(headers.call).toHaveBeenCalledWith(mocks)
+      expect(cacheControl.call).toHaveBeenCalledWith(mocks)
+      expect(contentSecurityPolicy.call).toHaveBeenCalledWith(mocks)
+    })
+
+    describe('when the last middleware returns a request and a response', () => {
+      it('returns the response', async () => {
+        contentSecurityPolicy.call.mockResolvedValue({
+          request: mocks.request,
+          response: mocks.response
+        })
+        result = handler(event)
+        await expect(result).resolves.toBe(mocks.response)
+      })
+    })
+
+    describe('when the last middleware returns only a request', () => {
+      it('returns the request', async () => {
+        contentSecurityPolicy.call.mockResolvedValue({
+          request: mocks.request,
+          response: undefined
+        })
+        result = handler(event)
+        await expect(result).resolves.toBe(mocks.request)
       })
     })
   })
