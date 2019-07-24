4208b0e3f8599d5753aedef7855b554f34d97924
Ian Purvis <ian.c.purvis@gmail.com>
2019-07-24 19:43:19 -0400

    Renames specs to spec

--- specs/fixtures/index.js
+++
@@ -1,2 +0,0 @@
-export { default as originRequestEvent } from '~/specs/fixtures/cloudfront-origin-request.js'
-export { default as originResponseEvent } from '~/specs/fixtures/cloudfront-origin-response.js'
