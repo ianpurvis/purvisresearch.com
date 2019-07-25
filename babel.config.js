defa909f3754a7fc47a18ecac862f89a193e5b77
Ian Purvis <ian.c.purvis@gmail.com>
2019-07-24 20:01:38 -0400

    Installs @babel/plugin-syntax-dynamic-import and  babel-plugin-dynamic-import-node
    Fixes coverage for missed files that use dynamic imports

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,32 @@
 module.exports = (api) => {
-  api.cache(true)
+  api.assertVersion('^7.0')
 
-  return {
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: {
-            node: 'current',
-          },
-        },
+  let config = {}
+
+  if (api.env('test')) {
+    config = {
+      plugins: [
+        '@babel/plugin-syntax-dynamic-import',
+        'babel-plugin-dynamic-import-node',
       ],
-    ],
+      presets: [
+        ['@babel/preset-env', {
+          corejs: 3,
+          modules: 'commonjs',
+          targets: {
+            node: 'current'
+          },
+          useBuiltIns: 'usage'
+        }]
+      ]
+    }
+  } else {
+    config = {
+      presets: [
+        '@vue/babel-preset-app'
+      ]
+    }
   }
+
+  return config
 }
