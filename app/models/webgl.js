0acd434f84d9a1c26f46725c2d35e49b6b8bf749
Ian Purvis <ian.c.purvis@gmail.com>
2019-08-07 11:12:38 -0500

    Adds webgl model with assertive availability detection

---
+++ app/models/webgl.js
@@ -0,0 +1,30 @@
+/* WebGL detection module
+ * Based on code from three.js examples:
+ *  https://github.com/mrdoob/three.js/blob/dev/examples/jsm/WebGL.js
+ */
+
+function  assertWebGLAvailable(canvas) {
+  if (!WebGL.isWebGLAvailable(canvas)) throw new WebGL.WebGLNotAvailableError()
+}
+
+function isWebGLAvailable(canvas) {
+  canvas = canvas || document.createElement('canvas')
+  try {
+    return !! (window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')))
+  } catch {
+    return false
+  }
+}
+
+class WebGLNotAvailableError extends Error {
+  constructor() {
+    super(`Your device does not seem to support WebGL.
+Learn more at http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation`)
+  }
+}
+
+export const WebGL = {
+  assertWebGLAvailable,
+  isWebGLAvailable,
+  WebGLNotAvailableError
+}
