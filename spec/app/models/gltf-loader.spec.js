9aecfaa154f12f35dbdcf3611f39ee32ed51e745
Ian Purvis <ian.c.purvis@gmail.com>
2019-08-26 12:17:29 -0500

    Adds gltf-loader spec

---
+++ spec/app/models/gltf-loader.spec.js
@@ -0,0 +1,48 @@
+jest.mock('three/examples/jsm/loaders/GLTFLoader.js')
+import { GLTFLoader as THREEGLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { GLTFLoader } from '~/models/gltf-loader.js'
+
+describe('GLTFLoader', () => {
+  let loader
+
+  it('extends THREE.GLTFLoader', () => {
+    expect(GLTFLoader.prototype).toBeInstanceOf(THREEGLTFLoader)
+  })
+
+  describe('parse(data)', () => {
+    let data, result
+
+    beforeEach(() => {
+      loader = new GLTFLoader()
+      data = 'mockData'
+    })
+    afterEach(() => {
+      expect(THREEGLTFLoader.prototype.parse).toHaveBeenCalledWith(
+        data,
+        '/',
+        expect.any(Function),
+        expect.any(Function)
+      )
+    })
+    describe('when super.parse() succeeds', () => {
+      it('resolves with a GLTF object', async () => {
+        let mockGLTF = 'mock gltf'
+        THREEGLTFLoader.prototype.parse =
+          jest.fn((data, path, onLoad, onError) => onLoad(mockGLTF))
+
+        result = loader.parse(data)
+        await expect(result).resolves.toBe(mockGLTF)
+      })
+    })
+    describe('when super.parse() fails', () => {
+      it('rejects with the error', async () => {
+        let mockError = new Error('mock error')
+        THREEGLTFLoader.prototype.parse =
+          jest.fn((data, path, onLoad, onError) => onError(mockError))
+
+        result = loader.parse(data)
+        await expect(result).rejects.toThrow(mockError)
+      })
+    })
+  })
+})
