8be778e7bc85b27becfd787d7ba088a44a9e0773
Ian Purvis <ian.c.purvis@gmail.com>
2019-08-27 17:22:58 -0500

    Adds smoke spec for oct 2017 page

---
+++ spec/app/pages/2017/oct.spec.js
@@ -0,0 +1,13 @@
+import { shallowMount } from '@vue/test-utils'
+import Component from '~/pages/2017/oct.vue'
+
+describe('oct.vue', () => {
+  let wrapper
+
+  it('is a Vue instance', () => {
+    wrapper = shallowMount(Component, {
+      stubs: ['nuxt-link']
+    })
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
