8bf4595f85e9e0bf4a3b141bcc40a724cfc07d94
Ian Purvis <ian.c.purvis@gmail.com>
2019-08-28 07:35:35 -0500

    Adds smoke spec for the index page

---
+++ spec/app/pages/index.spec.js
@@ -0,0 +1,13 @@
+import { shallowMount } from '@vue/test-utils'
+import Component from '~/pages/index.vue'
+
+describe('index.vue', () => {
+  let wrapper
+
+  it('is a Vue instance', () => {
+    wrapper = shallowMount(Component, {
+      stubs: ['nuxt-link']
+    })
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
