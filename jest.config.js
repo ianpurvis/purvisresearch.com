cbee2ea983b55f8bbab3e6c84aad603869874ca4
Ian Purvis <ian.c.purvis@gmail.com>
2019-08-28 07:31:29 -0500

    Configures jest for .glsl file mocking

--- jest.config.js
+++ jest.config.js
@@ -31,7 +31,7 @@ module.exports = {
         '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/spec/mocks/file-mock.js',
         '\\.(eot|otf|ttf|woff|woff2)$': '<rootDir>/spec/mocks/file-mock.js',
         '\\.(mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/spec/mocks/file-mock.js',
-        '\\.(gltf|glb)$': '<rootDir>/spec/mocks/file-mock.js',
+        '\\.(gltf|glb|glsl)$': '<rootDir>/spec/mocks/file-mock.js',
         '\\?as=file$': '<rootDir>/spec/mocks/file-mock.js',
         '~(.*)$': '<rootDir>/app/$1',
       },
