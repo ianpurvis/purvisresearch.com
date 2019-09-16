482c9cb795fdc3b66e45ea14e3e94d2da4d210f9
Ian Purvis <ian.c.purvis@gmail.com>
2019-09-16 12:47:04 -0500

    Simplifies CSP string compilation

--- lambda/middleware/content-security-policy.js
+++ lambda/middleware/content-security-policy.js
@@ -1,17 +1,58 @@
 import { LambdaEdgeHeaders } from './util.js'
 
-const NUXT_ROOT_SCRIPT_HASH = 'sha256-V/WaLGhSS+tTPAMDVjFgErm2VGPm+tNBC1rdDJHVkZ0='
+const DEFAULT_POLICY = `
+  base-uri
+    'none';
+  connect-src
+    'self'
+    https://www.google-analytics.com
+    https://sentry.io;
+  default-src
+    'none';
+  frame-ancestors
+    'none';
+  font-src
+    'self'
+    data:;
+  form-action
+    'none';
+  img-src
+    'self'
+    https://www.google-analytics.com;
+  manifest-src
+    'self';
+  media-src
+    'self';
+  object-src
+    'none';
+  script-src
+    'self'
+    'sha256-V/WaLGhSS+tTPAMDVjFgErm2VGPm+tNBC1rdDJHVkZ0='
+    https://www.google-analytics.com
+    https://sentry.io;
+  style-src
+    'self'
+    'unsafe-inline';
+`
 
 class ContentSecurityPolicy {
+
+  constructor(policy = DEFAULT_POLICY) {
+    this.policy = policy
+  }
 
   async call({ request, response }) {
     if (response && this.isHtml(response)) {
       const headers = LambdaEdgeHeaders({
-        'Content-Security-Policy': this.policyForNuxt()
+        'Content-Security-Policy': this.formatPolicy(this.policy)
       })
       response.headers = { ...response.headers, ...headers }
     }
     return { request, response }
+  }
+
+  formatPolicy(policy) {
+    return policy.replace(/\s+/g, ' ').trim()
   }
 
   isHtml(response) {
@@ -19,23 +60,6 @@ class ContentSecurityPolicy {
     const contentType = response.headers['content-type'][0].value
     const matcher = /^text\/html/i
     return matcher.test(contentType)
-  }
-
-  policyForNuxt(scriptHash = NUXT_ROOT_SCRIPT_HASH) {
-    return [
-      "base-uri 'none'",
-      "connect-src 'self' https://www.google-analytics.com https://sentry.io",
-      "default-src 'none'",
-      "frame-ancestors 'none'",
-      "font-src 'self' data:",
-      "form-action 'none'",
-      "img-src 'self' https://www.google-analytics.com",
-      "manifest-src 'self'",
-      "media-src 'self'",
-      "object-src 'none'",
-      `script-src 'self' '${scriptHash}' https://www.google-analytics.com https://sentry.io`,
-      "style-src 'self' 'unsafe-inline'",
-    ].join('; ')
   }
 }
 
