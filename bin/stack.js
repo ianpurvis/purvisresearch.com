bca4443c2610e3e7534601f7087f4a37f33b3c4d
Ian Purvis <ian.c.purvis@gmail.com>
2020-03-09 14:06:58 +0900

    Adds route53 hosted zone and cloudfront alias

--- bin/stack.js
+++ bin/stack.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
+require('dotenv').config()
 const cdk = require('@aws-cdk/core')
 const { Stack } = require('../lib/stack')
 
+const { ROUTE53_DOMAIN: domainName } = process.env
+
 const app = new cdk.App()
-new Stack(app, 'PurvisResearchStack')
+const stack = new Stack(app, 'PurvisResearchStack', {
+  domainName
+})
