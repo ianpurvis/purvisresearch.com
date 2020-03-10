725ae0ac38149035cb7af9a3fa4a1fdd2a5e89cd
Ian Purvis <ian.c.purvis@gmail.com>
2020-03-10 13:45:30 +0900

    Configures stack name via CLOUD_FORMATION_STACK_NAME

--- bin/stack.js
+++ bin/stack.js
@@ -4,11 +4,18 @@ require('dotenv').config()
 const cdk = require('@aws-cdk/core')
 const { Stack } = require('../lib/stack')
 
-const { ROUTE53_DOMAIN: domainName } = process.env
-const lambdaPath = 'dist/lambda'
+const {
+  ROUTE53_DOMAIN,
+  CLOUD_FORMATION_STACK_NAME
+} = process.env
+
+if (!ROUTE53_DOMAIN)
+  throw new Error("Missing environment variable 'ROUTE53_DOMAIN'")
+if (!CLOUD_FORMATION_STACK_NAME)
+  throw new Error("Missing environment variable 'CLOUD_FORMATION_STACK_NAME'")
 
 const app = new cdk.App()
-const stack = new Stack(app, 'PurvisResearchStack', {
-  domainName,
-  lambdaPath
+const stack = new Stack(app, CLOUD_FORMATION_STACK_NAME, {
+  domainName: ROUTE53_DOMAIN,
+  lambdaPath: 'dist/lambda'
 })
