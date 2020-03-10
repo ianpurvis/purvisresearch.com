0e448c77cdb4a9accd9825f061107b7f970d3175
Ian Purvis <ian.c.purvis@gmail.com>
2020-03-10 12:27:24 +0900

    Adds lambda@edge function to cdk deployment

--- bin/stack.js
+++ bin/stack.js
@@ -5,8 +5,10 @@ const cdk = require('@aws-cdk/core')
 const { Stack } = require('../lib/stack')
 
 const { ROUTE53_DOMAIN: domainName } = process.env
+const lambdaPath = 'dist/lambda'
 
 const app = new cdk.App()
 const stack = new Stack(app, 'PurvisResearchStack', {
-  domainName
+  domainName,
+  lambdaPath
 })
