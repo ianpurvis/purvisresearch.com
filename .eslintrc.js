54d9e464c2e8cfa55b16cf8cc218842b0a6a030f
Ian Purvis <ian.c.purvis@gmail.com>
2020-05-20 11:31:24 -0500

    Configures eslint to avoid escape quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -62,7 +62,7 @@ module.exports = {
   rules: {
     indent: [ 'error', 2 ],
     'linebreak-style': [ 'error', 'unix' ],
-    quotes: [ 'error', 'single' ],
+    quotes: [ 'error', 'single', { 'avoidEscape': true } ],
     semi: [ 'error', 'never' ]
   }
 }
