fd2d053292b9d0e50aefb4e307ecd4bf452991b4
Ian Purvis <ian.c.purvis@gmail.com>
2020-05-22 00:06:09 -0500

    Migrates nov 2017 demo to cannon physics worker

--- app/pages/2017/nov.js
+++ app/pages/2017/nov.js
@@ -1,20 +1,21 @@
 import {
   Font,
   LessDepth,
+  Mesh,
   MeshNormalMaterial,
   TextBufferGeometry,
-  Vector3
+  Vector3,
 } from 'three'
 import ogImagePath from '~/assets/images/2017/nov.png'
 import Inconsolata from '~/assets/models/Inconsolata_Regular.json'
 import ThreeDemo from '~/mixins/three-demo.js'
 import { DEGREES_TO_RADIANS } from '~/models/constants.js'
 import { Organization } from '~/models/organization.js'
-import { Particle } from '~/models/particle.js'
 import { Random } from '~/models/random.js'
 
 export default {
   beforeDestroy() {
+    this.worker.terminate()
   },
   created() {
     // Non-reactive data:
@@ -64,53 +65,103 @@ export default {
     layout() {
       this.camera.far = 10000
       this.camera.position.z = Random.rand({ min: 100, max: 150 })
-
-      const blastRadius = 60
-      const components = [ 'x', 'y', 'z' ]
-      let mass, acceleration, rotation
-      this.particles.forEach(particle => {
-        mass = Random.rand({ min: 0.25, max: 1 })
-        acceleration = components.map(() =>
-          Random.rand({ min: -blastRadius, max: blastRadius })
-        )
-        rotation = components.map(() =>
-          Random.rand({ max: 360 }) * DEGREES_TO_RADIANS
-        )
-        particle.mass = mass
-        particle.acceleration.set(...acceleration)
-        particle.rotation.set(...rotation)
-        // Scale particle according to mass:
-        particle.scale.setScalar(mass)
-        // Give particle a jump start in the explosion:
-        particle.position.set(...acceleration)
-      })
+      this.camera.updateProjectionMatrix()
     },
-    load() {
-      return Promise.resolve(
-        ThreeDemo.methods.load.call(this)
-      ).then(() => {
-        const font = new Font(Inconsolata)
-        const material = new MeshNormalMaterial({
-          depthFunc: LessDepth,
-          opacity: 0.7,
-          transparent: false,
-          wireframe: true,
-          wireframeLinewidth: 2.0,
-        })
-
-        this.particles = Array
-          .from('abcdefghijklmnopqrstuvwxyz0123456789')
-          .map(character =>
-            new TextBufferGeometry(character, { font }).center()
-          )
-          .map(geometry => new Particle(geometry, material))
-
-        this.scene.add(...this.particles)
+    async load() {
+      await ThreeDemo.methods.load.call(this)
+      this.loadGraphics()
+      this.loadPhysics()
+    },
+    loadGraphics() {
+      const blastRadius = 60
+      const font = new Font(Inconsolata)
+      const characters = Array.from('abcdefghijklmnopqrstuvwxyz0123456789')
+      const geometries = characters.map(character => new TextBufferGeometry(character, { font }))
+      geometries.forEach(geometry => {
+        geometry.center()
+        geometry.computeBoundingBox()
       })
+      const material = new MeshNormalMaterial({
+        depthFunc: LessDepth,
+        opacity: 0.7,
+        transparent: false,
+        wireframe: true,
+        wireframeLinewidth: 2.0,
+      })
+      const meshes = geometries.map(geometry => new Mesh(geometry, material))
+      meshes.forEach(mesh => {
+        mesh.rotation.set(
+          Random.rand({ max: 360 }) * DEGREES_TO_RADIANS,
+          Random.rand({ max: 360 }) * DEGREES_TO_RADIANS,
+          Random.rand({ max: 360 }) * DEGREES_TO_RADIANS,
+        )
+        mesh.position.set(
+          Random.rand({ min: -blastRadius, max: blastRadius }),
+          Random.rand({ min: -blastRadius, max: blastRadius }),
+          Random.rand({ min: -blastRadius, max: blastRadius }),
+        )
+        mesh.scale.setScalar(Random.rand({ min: 0.25, max: 1 }))
+        // mesh.visible = false
+        this.scene.add(mesh)
+      })
+
+      Object.assign(this, { meshes })
+    },
+    loadPhysics() {
+      this.worker = new Worker('~/workers/exploder-physics-worker.js', { type:'module' })
+      this.worker.onmessage = this.onmessage.bind(this)
+      const size = new Vector3()
+      const bodies = this.meshes.map(mesh => {
+        mesh.geometry.boundingBox.getSize(size)
+        return {
+          collisionFilterGroup: 2,
+          collisionFilterMask: 1,
+          mass: mesh.scale.x,
+          position: {
+            x: mesh.position.x,
+            y: mesh.position.y,
+            z: mesh.position.z,
+          },
+          quaternion: {
+            x: mesh.quaternion.x,
+            y: mesh.quaternion.y,
+            z: mesh.quaternion.z,
+            w: mesh.quaternion.w,
+          },
+          size
+        }
+      })
+      // size.setScalar(60)
+      // const blast = {
+      //   collisionFilterGroup: 1,
+      //   collisionFilterMask: 2,
+      //   mass: 10000,
+      //   size
+      // }
+      // bodies.push(blast)
+      this.worker.postMessage({ command: 'load', args: { bodies }})
+    },
+    onmessage({ data: { event, args }}) {
+      if (this[event]) this[event](args)
+    },
+    onload() {
+      this.workerIsLoaded = true
+    },
+    onstep({ bodies }) {
+      for (let i = 0, mesh, body; i < this.meshes.length; i++) {
+        mesh = this.meshes[i]
+        body = bodies[i]
+        mesh.position.copy(body.position)
+        mesh.quaternion.copy(body.quaternion)
+        mesh.visible = true
+      }
     },
     update() {
+      if (!this.workerIsLoaded) return
       ThreeDemo.methods.update.call(this)
-      this.particles.forEach(p => p.update(this.deltaTime))
+      this.worker.postMessage({ command: 'step', args: {
+        deltaTime: this.deltaTime * this.speedOfLife
+      }})
     },
   },
   mixins: [
