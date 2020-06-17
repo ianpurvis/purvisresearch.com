82417ed376c5e42296454d03aa16de1fae9b2255
Ian Purvis <ian.c.purvis@gmail.com>
2020-06-16 22:28:45 -0500

    Adds impact coefficient to wind force

--- app/models/flag-physics-wind.js
+++ app/models/flag-physics-wind.js
@@ -4,6 +4,7 @@ import { SimplexNoise } from 'three/examples/jsm/math/SimplexNoise.js'
 class FlagPhysicsWind {
 
   constructor() {
+    this.deltaTime = 0
     this.force = new Ammo.btVector3()
     this.noiseMaker = new SimplexNoise()
     this.velocity = new Ammo.btVector3()
@@ -15,19 +16,29 @@ class FlagPhysicsWind {
   }
 
   applyToSoftBody(body) {
-    for (let i = 0, size = body.m_nodes.size(), vertex, noise; i < size; i++) {
+    for (
+      let i = 0, size = body.m_nodes.size(), node, noise, impact;
+      i < size;
+      i++
+    ) {
+      node = body.m_nodes.at(i)
+
       this.force.setValue(
         this.velocity.x(),
         this.velocity.y(),
         this.velocity.z()
       )
-      vertex = body.m_nodes.at(i).m_x
+
       noise = this.noiseMaker.noise3d(
-        vertex.x(),
-        vertex.y(),
+        node.m_x.x(),
+        node.m_x.y(),
         this.deltaTime
       )
       this.force.op_mul(noise)
+
+      impact = Math.abs(this.force.dot(node.m_n))
+      this.force.op_mul(impact)
+
       body.addForce(this.force, i)
     }
   }
