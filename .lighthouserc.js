e329f1b21ac73381ce4f0e7031d4707ce5447583
Ian Purvis <ian.c.purvis@gmail.com>
2020-07-12 20:34:10 -0500

    Removes assertions from lighthouseci config

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,16 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      extends: 'lighthouse:default',
       numberOfRuns: 1,
-      settings: {
-        onlyCategories: [
-          'performance',
-          'accessibility',
-          'best-practices',
-          'seo'
-        ],
-      },
       staticDistDir: 'dist/app',
     },
   }
