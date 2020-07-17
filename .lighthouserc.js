7fd8e95f11534d620a40ad66a8a5f63016413b92
Ian Purvis <ian.c.purvis@gmail.com>
2020-07-16 19:16:27 -0500

    Removes assertions from lhci config

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,18 +2,6 @@ require('dotenv').config()
 
 module.exports = {
   ci: {
-    assert: {
-      preset: 'lighthouse:no-pwa',
-      assertions: {
-        'canonical': 'warn',
-        'color-contrast': 'warn',
-        'errors-in-console': 'warn',
-        'font-display': 'warn',
-        'tap-targets': 'warn',
-        'unused-javascript': 'warn',
-        'uses-rel-preconnect': 'warn',
-      }
-    },
     collect: {
       numberOfRuns: 1,
       staticDistDir: 'dist/app',
