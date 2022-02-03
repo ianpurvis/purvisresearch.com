f276181e4d83f893434d9d4f3dae6edf9ebdf6b1
Ian Purvis <ian@purvisresearch.com>
2022-02-03 11:10:12 -0700

    Migrate sept 2017 to PixiEngine and EmojiParticleFlowScene

--- app/pages/2017/sept.js
+++ app/pages/2017/sept.js
@@ -1,14 +1,16 @@
 import ogImagePath from '~/assets/images/2017/sept/an-emoji-particle-flow-in-webgl.png'
-import PixiDemo from '~/mixins/pixi-demo.js'
-import { SECONDS_TO_MILLISECONDS } from '~/models/constants.js'
+import Graphix from '~/mixins/graphix.js'
 import { Organization } from '~/models/organization.js'
 
 export default {
+  beforeDestroy() {
+    this.dispose()
+  },
   created() {
     // Non-reactive data:
     this.canonicalUrl = `${Organization.default.url}/2017/sept.html`
+    this.title = 'Sep 2017: An Emoji Particle Flow in WebGL | Purvis Research'
     this.description = 'An emoji particle flow in WebGL.'
-    this.emitter = null
     this.jsonld = {
       '@context': 'https://schema.org',
       '@type': 'BreadcrumbList',
@@ -23,10 +25,7 @@ export default {
         'name': 'sept 2017',
         'item': this.canonicalUrl
       }]
-    },
-    this.speedOfLife = 0.4 // Slow-motion
-    this.textures = []
-    this.title = 'Sep 2017: An Emoji Particle Flow in WebGL | Purvis Research'
+    }
   },
   head() {
     return {
@@ -51,103 +50,29 @@ export default {
   },
   methods: {
     dispose() {
-      this.textures.forEach(texture => texture.destroy())
-      if (this.emitter) this.emitter.destroy()
-      PixiDemo.methods.dispose.call(this)
+      this.engine.dispose()
     },
     async load() {
-      await PixiDemo.methods.load.call(this)
-      const [
-        { RenderTexture, Text },
-        { Emitter }
-      ] = await Promise.all([
-        import('~/shims/pixi.js'),
-        import('pixi-particles')
-      ])
-
-      Array
-        .from('💾📀')
-        .map(emoji => new Text(emoji, {
-          fontSize: '32px',
-        }))
-        .forEach(text => {
-          // Pre-render texture and discard the text:
-          let texture = RenderTexture.create({
-            height: 32,
-            width: 32,
-            resolution: this.renderer.resolution
-          })
-          this.renderer.render(text, texture)
-          this.textures.push(texture)
-          text.destroy(true)
-        })
-
-      let options = {
-        autoUpdate: false,
-        frequency: 0.020,
-        pos: {
-          x: 0,
-          y: 0
-        },
-        rotationSpeed: {
-          min: 100,
-          max: 400
-        },
-        scale: {
-          list: [
-            {
-              value: 0.5,
-              time: 0
-            },{
-              value: 1.25,
-              time: 1
-            }
-          ],
-        },
-        spawnType: 'rect',
-        startRotation: {
-          min: -5,
-          max: 5
-        }
-      }
-
-      let { height, width } = this.frame()
-      options.spawnRect = {
-        h: height
-      }
-
-      let speed = 100 // approx px per second
-      options.speed = {
-        list: [
-          {
-            value: speed,
-            time: 0
-          },{
-            value: speed * 3.0,
-            time: 1
-          }
-        ],
-      }
-
-      // Set lifetime to be a little less than width / avg speed
-      let lifetime = width / (speed * 1.8)
-      options.lifetime = {
-        min: lifetime,
-        max: lifetime
-      }
-
-      this.emitter = new Emitter(this.scene, this.textures, options)
-    },
-    update() {
-      PixiDemo.methods.update.call(this)
-      if (!this.emitter) return
-      this.emitter.update(this.deltaTime / SECONDS_TO_MILLISECONDS)
+      const canvas = this.$refs.canvas
+      const { PixiEngine } = await import('../../engines/pixi-engine.js')
+      const { EmojiParticleFlowScene } = await import('../../scenes/emoji-particle-flow.js')
+      const engine = new PixiEngine(canvas)
+      const { clientHeight, clientWidth } = canvas
+      const scene = new EmojiParticleFlowScene(clientWidth, clientHeight)
+      engine.stage.addChild(scene)
+      engine.onUpdate = (deltaTime) => scene.update(deltaTime)
+      this.engine = engine
     }
   },
   mixins: [
-    PixiDemo,
+    Graphix,
   ],
   async mounted() {
-    await this.load().catch(this.logError)
+    try {
+      await this.load()
+      this.engine.play()
+    } catch (error) {
+      this.logError(error)
+    }
   }
 }
