f6518bf09aa790ef6a6bb4bcc85b489f8c10d69c
Ian Purvis <ian@purvisresearch.com>
2022-02-03 11:05:02 -0700

    Add logError() to Graphix mixin

--- app/mixins/graphix.js
+++ app/mixins/graphix.js
@@ -1,5 +1,6 @@
 import Tight from 'vue-tight'
 import Unobfuscate from '~/directives/unobfuscate.js'
+import { WebGL } from '~/models/webgl.js'
 
 export default {
   directives: {
@@ -7,9 +8,16 @@ export default {
     Unobfuscate
   },
   methods: {
+    logError(error) {
+      if (error instanceof WebGL.WebGLNotAvailableError) {
+        console.warn(error.message)
+      } else {
+        this.$sentry.captureException(error)
+        console.error(error)
+      }
+    },
     snake(value) {
       return value.replace(/\s/g, `<span class="underscore" ${this.$options._scopeId}>&nbsp;</span>`)
     },
   }
 }
-
