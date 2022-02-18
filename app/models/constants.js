69fd749376447d685b7dd58ae23877ec6fc1ab05
Ian Purvis <ian@purvisresearch.com>
2022-02-18 13:18:06 -0700

    Add RADIANS_TO_DEGREES to constants

--- app/models/constants.js
+++ app/models/constants.js
@@ -1,2 +1,3 @@
-export const DEGREES_TO_RADIANS = (Math.PI / 180)
+export const DEGREES_TO_RADIANS = Math.PI / 180
+export const RADIANS_TO_DEGREES = 180 / Math.PI
 export const SECONDS_TO_MILLISECONDS = 1000
