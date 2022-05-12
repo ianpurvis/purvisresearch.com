61d660a58a6120d59f6d3d7483f8e412b67d0dab
Ian Purvis <ian@purvisresearch.com>
2022-05-12 12:55:14 -0500

    Migrate jul 2020 to new Three engine

--- app/pages/2020/jul.js
+++ app/pages/2020/jul.js
@@ -1,26 +1,18 @@
-import {
-  DoubleSide,
-  DynamicDrawUsage,
-  Mesh,
-  MeshBasicMaterial,
-  PlaneBufferGeometry,
-} from 'three'
-import ogImagePath from '~/assets/images/2020/jul/a-banknote-in-simplex-wind.png'
-import billImagePath from '~/assets/images/2020/jul/tubman-twenty.jpg'
-import ThreeDemo from '~/mixins/three-demo.js'
-import { ChaseCameraRig } from '~/models/chase-camera-rig.js'
-import { Organization } from '~/models/organization.js'
-import { Oscillator } from '~/models/oscillator.js'
-import { TextureLoader } from '~/models/texture-loader.js'
-import { DollarPhysicsWorker } from '~/workers/dollar-physics-worker.js'
+import ogImagePath from '../../assets/images/2020/jul/a-banknote-in-simplex-wind.png'
+import { ThreeEngine } from '../../engines/three-engine.js'
+import Graphix from '../../mixins/graphix.js'
+import { Organization } from '../../models/organization.js'
+import { BanknoteInSimplexWind } from '../../scenes/banknote-in-simplex-wind.js'
+import { WebGL } from '../../models/webgl.js'
 
 export default {
   beforeDestroy() {
-    this.physicsWorker.terminate()
+    this.dispose()
   },
   created() {
     // Non-reactive data:
     this.canonicalUrl = `${Organization.default.url}/2020/jul.html`
+    this.title = 'Jul 2020: A Banknote in Simplex Wind | Purvis Research'
     this.description = 'A Banknote In Simplex Wind'
     this.jsonld = {
       '@context': 'https://schema.org',
@@ -37,8 +29,6 @@ export default {
         'item': this.canonicalUrl
       }]
     }
-    this.ogImageUrl = `${Organization.default.url}${ogImagePath}`
-    this.title = 'Jul 2020: A Banknote in Simplex Wind | Purvis Research'
   },
   head() {
     return {
@@ -46,7 +36,7 @@ export default {
       meta: [
         { name: 'description', content: this.description, hid: 'description' },
         { property:'og:description', content: this.description },
-        { property:'og:image', content: this.ogImageUrl },
+        { property:'og:image', content: `${Organization.default.url}${ogImagePath}` },
         { property:'og:image:height', content:'859' },
         { property:'og:image:width', content:'1646' },
         { property:'og:title', content:'Jul 2020' },
@@ -62,98 +52,23 @@ export default {
     }
   },
   methods: {
-    ...ThreeDemo.methods,
+    dispose() {
+      if (this.scene) this.scene.dispose()
+      if (this.engine) this.engine.dispose()
+    },
     async load() {
-      await ThreeDemo.methods.load.call(this)
-      await this.loadBill()
-      this.loadCamera()
-      this.loadPhysics()
-    },
-    loadCamera() {
-      this.camera.far = 1000
-
-      const cameraRig = new ChaseCameraRig(this.camera, this.mesh)
-      cameraRig.position.z = cameraRig.offset.z = 12
-      this.scene.add(cameraRig)
-
-      const chaseOscillator = new Oscillator({
-        period: 20, // seconds
-        amplitude: 0.1,
-        yshift: 0.65,
-      })
-
-      Object.assign(this, { cameraRig, chaseOscillator })
-    },
-    async loadBill() {
-      const geometry = new PlaneBufferGeometry(
-        15.61,  // width = 156.1mm / 1000 mm per m * 100 scale
-        6.63,   // height = 66.3mm / 1000 mm per m * 100 scale
-        15,     // widthSegments
-        6,      // heightSegments
-      )
-      geometry.deleteAttribute('normal')
-      const textureLoader = new TextureLoader()
-      const billTexture = await textureLoader.load(billImagePath)
-      billTexture.repeat.y = 0.806640625 // 826px / 1024px padded height
-      const material = new MeshBasicMaterial({
-        map: billTexture,
-        side: DoubleSide,
-      })
-      const mesh = new Mesh(geometry, material)
-      mesh.frustumCulled = false
-
-      this.scene.add(mesh)
-
-      Object.assign(this, { mesh })
-    },
-    loadPhysics() {
-      const physicsWorker = new DollarPhysicsWorker()
-      physicsWorker.onload = this.onload.bind(this)
-      physicsWorker.onstep = this.onstep.bind(this)
-      physicsWorker.onerror = this.logError.bind(this)
-      Object.assign(this, { physicsWorker })
-
-      physicsWorker.load({
-        mass: 0.1, // 1g / 1000 g per kg * 100 scale
-        vertices: this.mesh.geometry.attributes.position.array,
-        triangles: this.mesh.geometry.index.array
-      })
-    },
-    onload({ vertices, triangles }) {
-      this.mesh.geometry.index.array = triangles
-      this.mesh.geometry.attributes.position.array = vertices
-      // Optimize usage for stepped drawing:
-      this.mesh.geometry.attributes.position.usage = DynamicDrawUsage
-      this.mesh.geometry.attributes.position.needsUpdate = true
-      window.requestAnimationFrame(this.update)
-    },
-    onstep({ vertices }) {
-      this.mesh.geometry.attributes.position.array = vertices
-      this.mesh.geometry.attributes.position.needsUpdate = true
-      window.requestAnimationFrame(this.update)
-    },
-    startAnimating() {
-      this.clock.start()
-    },
-    update() {
-      this.deltaTime = this.clock.getDelta() * this.speedOfLife
-      this.elapsedTime += this.deltaTime
-
-      this.cameraRig.smoothing = this.chaseOscillator.cos(this.elapsedTime)
-      this.cameraRig.update(this.deltaTime)
-
-      this.render()
-
-      this.physicsWorker.step({
-        deltaTime: this.deltaTime,
-        vertices: this.mesh.geometry.attributes.position.array,
-      })
-    },
+      const { canvas } = this.$refs
+      WebGL.assertWebGLAvailable(canvas)
+      const engine = this.engine = new ThreeEngine(canvas, { maxFPS: 0 })
+      const scene = engine.scene = new BanknoteInSimplexWind()
+      await scene.load()
+      await engine.play()
+    }
   },
   mixins: [
-    ThreeDemo
+    Graphix
   ],
-  async mounted() {
-    await this.load().catch(this.logError)
+  mounted() {
+    this.load().catch(Graphix.errorCaptured)
   }
 }
