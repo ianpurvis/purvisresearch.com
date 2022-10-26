5f4ce41d263eb7ef4cc34acdf582c5a9a5209f7b
Ian Purvis <ian@purvisresearch.com>
2022-10-26 15:40:46 -0500

    Add cloudfront response headers policy to terraform stack

--- infra/gateway.tf
+++ infra/gateway.tf
@@ -1,6 +1,78 @@
 resource "aws_cloudfront_origin_access_identity" "gateway" {
 }
 
+resource "aws_cloudfront_response_headers_policy" "gateway" {
+  name = "gateway-policy"
+  security_headers_config {
+    content_security_policy {
+      content_security_policy = <<-EOF
+        base-uri
+          'none';
+        child-src
+          'self'
+          blob:;
+        connect-src
+          'self'
+          https://www.google-analytics.com
+          https://sentry.io;
+        default-src
+          'none';
+        frame-ancestors
+          'none';
+        font-src
+          'self'
+          data:;
+        form-action
+          'none';
+        img-src
+          'self'
+          https://www.google-analytics.com;
+        manifest-src
+          'self';
+        media-src
+          'self';
+        object-src
+          'none';
+        script-src
+          'self'
+          'unsafe-eval'
+          'unsafe-inline'
+          https://www.google-analytics.com
+          https://sentry.io;
+        style-src
+          'self'
+          'unsafe-inline';
+        worker-src
+          'self'
+          blob:;
+      EOF
+      override = true
+    }
+    content_type_options {
+      override = true
+    }
+    frame_options {
+      frame_option = "DENY"
+      override = true
+    }
+    referrer_policy {
+      override = true
+      referrer_policy = "no-referrer-when-downgrade"
+    }
+    strict_transport_security {
+      access_control_max_age_sec = 31556952
+      include_subdomains = true
+      override = true
+      preload = true
+    }
+    xss_protection {
+      mode_block = true
+      override = true
+      protection = true
+    }
+  }
+}
+
 resource "aws_cloudfront_distribution" "gateway" {
   aliases = [
     aws_acm_certificate.root.domain_name
