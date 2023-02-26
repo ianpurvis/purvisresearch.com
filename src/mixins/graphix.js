4b5810dbe102ed120c1b48cbd4200c12653fe22d
Ian Purvis <ian@purvisresearch.com>
2023-02-26 13:39:39 -0600

    Migrate pages to detectWebGL

--- src/mixins/graphix.js
+++ src/mixins/graphix.js
@@ -1,15 +1,7 @@
 import Tight from 'vue-tight'
 import Unobfuscate from '~/directives/unobfuscate.js'
-import { WebGL } from '~/models/webgl.js'
 
 export default {
-  errorCaptured(error) {
-    if (error instanceof WebGL.WebGLNotAvailableError) {
-      console.warn(error.message)
-      return false
-    }
-    throw error
-  },
   directives: {
     Tight,
     Unobfuscate
