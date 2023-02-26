1bb5148df4413a52c933c03531a62301c4d8bea8
Ian Purvis <ian@purvisresearch.com>
2023-02-26 14:34:42 -0600

    Migrate sept 2017 to the composition api

--- src/pages/2017/sept.vue
+++ src/pages/2017/sept.vue
@@ -4,4 +4,78 @@
   </main>
 </template>
 
-<script src="./sept.js"></script>
+<script>
+import { onBeforeUnmount, onMounted, ref } from 'vue'
+import ogImagePath from '~/assets/images/2017/sept/an-emoji-particle-flow-in-webgl.png'
+import Graphix from '~/components/graphix.vue'
+import { Organization } from '~/models/organization.js'
+import { detectWebGL } from '~/models/webgl.js'
+
+const org = Organization.default
+const canonicalUrl = `${org.url}/2017/sept.html`
+const title = 'Sep 2017: An Emoji Particle Flow in WebGL | Purvis Research'
+const description = 'An emoji particle flow in WebGL.'
+const jsonld = JSON.stringify({
+  '@context': 'https://schema.org',
+  '@type': 'BreadcrumbList',
+  'itemListElement': [{
+    '@type': 'ListItem',
+    'position': 1,
+    'name': 'purvis research',
+    'item': org.url
+  },{
+    '@type': 'ListItem',
+    'position': 2,
+    'name': 'sept 2017',
+    'item': canonicalUrl
+  }]
+})
+
+export default {
+  components: {
+    Graphix
+  },
+  head() {
+    return {
+      title,
+      meta: [
+        { name: 'description', content: description, hid: 'description' },
+        { property:'og:description', content: description },
+        { property:'og:image', content: `${Organization.default.url}${ogImagePath}` },
+        { property:'og:image:height', content:'859' },
+        { property:'og:image:width', content:'1646' },
+        { property:'og:title', content:'Sept 2017' },
+        { property:'og:url', content: canonicalUrl },
+        { name:'twitter:card', content:'summary_large_image' },
+      ],
+      link: [
+        { rel: 'canonical', href: canonicalUrl }
+      ],
+      script: [
+        { type: 'application/ld+json', json: jsonld }
+      ],
+    }
+  },
+  setup() {
+    const graphix = ref(null)
+    let engine
+
+    onMounted(async () => {
+      const { value: { $refs: { canvas } } } = graphix
+      if (!detectWebGL(canvas)) return
+      const { PixiEngine } = await import('../../engines/pixi-engine.js')
+      const { EmojiParticleFlowScene } = await import('../../scenes/emoji-particle-flow.js')
+      engine = new PixiEngine(canvas, { maxFPS: 30 })
+      const { clientHeight, clientWidth } = canvas
+      engine.scene = new EmojiParticleFlowScene(clientWidth, clientHeight)
+      await engine.play()
+    })
+
+    onBeforeUnmount(() => {
+      if (engine) engine.dispose()
+    })
+
+    return { description, graphix }
+  }
+}
+</script>
