f90cac868b0c64609e29260794c77276e3100b48
Ian Purvis <ian@purvisresearch.com>
2023-02-26 14:43:58 -0600

    Migrate oct 2017 to the composition api

--- spec/pages/2017/oct.spec.js
+++ spec/pages/2017/oct.spec.js
@@ -1,5 +1,4 @@
-import Page from '~/pages/2017/oct.vue'
-import * as metadata from '~/pages/2017/oct.js'
+import Page, * as metadata from '~/pages/2017/oct.vue'
 import { describePage } from '../../examples/page.js'
 
 describePage('Oct 2017', Page, metadata)
