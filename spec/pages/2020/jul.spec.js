4e9590f3537f6d92daec59e3ae9a827d7f4be463
Ian Purvis <ian@purvisresearch.com>
2023-02-27 09:15:28 -0600

    Migrate jul 2020 page to the composition api

--- spec/pages/2020/jul.spec.js
+++ spec/pages/2020/jul.spec.js
@@ -1,5 +1,4 @@
-import Page from '~/pages/2020/jul.vue'
-import * as metadata from '~/pages/2020/jul.js'
+import Page, * as metadata from '~/pages/2020/jul.vue'
 import { describePage } from '../../examples/page.js'
 
 describePage('Jul 2020', Page, metadata)
