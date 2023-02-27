65cc683a0d0e95becbab9950ded8264b2f16320c
Ian Purvis <ian@purvisresearch.com>
2023-02-26 18:07:05 -0600

    Migrate apr 2019 page to the composition api

--- spec/pages/2019/apr.spec.js
+++ spec/pages/2019/apr.spec.js
@@ -1,5 +1,4 @@
-import Page from '~/pages/2019/apr.vue'
-import * as metadata from '~/pages/2019/apr.js'
+import Page, * as metadata from '~/pages/2019/apr.vue'
 import { describePage } from '../../examples/page.js'
 
 describePage('Apr 2019', Page, metadata)
