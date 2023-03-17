7bf17c67e85f83da78555009023b60b4958e56b3
Ian Purvis <ian@purvisresearch.com>
2023-03-17 12:43:44 -0500

    Add shared example specs

---
+++ spec/examples/page.js
@@ -0,0 +1,25 @@
+import { shallowMount } from '@vue/test-utils'
+import { beforeAll, beforeEach, describe, expect, it } from 'jest-ctx'
+import { MESSAGE_NO_WEBGL } from '~/models/webgl.js'
+import { mockConsole } from './console.js'
+import { describeVueMetaBehavior } from './vue-meta.js'
+
+export function describePage(name, Page, metadata) {
+  describe(name, () => {
+    mockConsole()
+
+    beforeAll(() => ({ stubs: ['nuxt-link'] }))
+    beforeEach(() => { console.warn.mockClear() })
+    beforeEach((options) => shallowMount(Page, options))
+
+    it('mounts', (wrapper) => {
+      expect(wrapper.exists()).toBeTruthy()
+    })
+
+    it('warns if webgl is not available', () => {
+      expect(console.warn).toHaveBeenCalledWith(MESSAGE_NO_WEBGL)
+    })
+
+    describeVueMetaBehavior(metadata)
+  })
+}
