6453ffd1fe7ebb5c42fdbb01e838729eb2efdac8
Ian Purvis <ian@purvisresearch.com>
2023-04-05 11:45:26 -0500

    chore: Migrate Oct 2017 page to composition api (#889)
    * Export metadata from oct 2017 page
    
    * Migrate oct 2017 page to the composition api

--- src/pages/2017/oct.vue
+++ src/pages/2017/oct.vue
@@ -4,4 +4,80 @@
   </main>
 </template>
 
-<script src="./oct.js"></script>
+<script>
+import { onBeforeUnmount, onMounted, ref } from 'vue'
+import ogImagePath from '~/assets/images/2017/oct/a-bezier-moire-generator-in-webgl.png'
+import Graphix from '~/components/graphix.vue'
+import { Organization } from '~/models/organization.js'
+import { detectWebGL } from '~/models/webgl.js'
+
+const org = Organization.default
+export const canonicalUrl = `${org.url}/2017/oct.html`
+export const title = 'Oct 2017: A Bézier Moiré Generator in WebGL | Purvis Research'
+export const description = 'A bézier moiré generator in WebGL.'
+export const ogImageUrl = `${org.url}${ogImagePath}`
+export const ogTitle = 'Oct 2017'
+export const jsonld = JSON.stringify({
+  '@context': 'https://schema.org',
+  '@type': 'BreadcrumbList',
+  'itemListElement': [{
+    '@type': 'ListItem',
+    'position': 1,
+    'name': 'purvis research',
+    'item': org.url
+  },{
+    '@type': 'ListItem',
+    'position': 2,
+    'name': 'oct 2017',
+    'item': canonicalUrl
+  }]
+})
+
+export default {
+  components: {
+    Graphix
+  },
+  head () {
+    return {
+      title,
+      meta: [
+        { name: 'description', content: description, hid: 'description' },
+        { property:'og:description', content: description },
+        { property:'og:image', content: ogImageUrl },
+        { property:'og:image:height', content:'859' },
+        { property:'og:image:width', content:'1646' },
+        { property:'og:title', content: ogTitle },
+        { property:'og:url', content: canonicalUrl },
+        { name:'twitter:card', content:'summary_large_image' },
+      ],
+      link: [
+        { rel: 'canonical', href: canonicalUrl }
+      ],
+      script: [
+        { type: 'application/ld+json', json: jsonld }
+      ],
+    }
+  },
+  setup() {
+    const graphix = ref(null)
+    let engine
+
+    onMounted(async () => {
+      const { value: { $refs: { canvas } } } = graphix
+      if (!detectWebGL(canvas)) return
+      const { PixiEngine } = await import('../../engines/pixi-engine.js')
+      const { BezierMoireGenerator } = await import('../../scenes/bezier-moire-generator.js')
+      engine = new PixiEngine(canvas, { maxFPS: 30 })
+      const { clientHeight, clientWidth } = canvas
+      engine.scene = new BezierMoireGenerator(clientWidth, clientHeight)
+      await engine.play()
+    })
+
+    onBeforeUnmount(() => {
+      if (engine) engine.dispose()
+    })
+
+    return { description, graphix }
+  }
+}
+</script>
