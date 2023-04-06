e07bed4203d12f96dc78a30650689a2ecf36fa7f
Ian Purvis <ian@purvisresearch.com>
2023-04-06 07:15:42 -0500

    Export metadata from jul 2020 page

---
+++ spec/pages/2020/jul.spec.js
@@ -0,0 +1,5 @@
+import Page from '~/pages/2020/jul.vue'
+import * as metadata from '~/pages/2020/jul.js'
+import { describePage } from '../../examples/page.js'
+
+describePage('Jul 2020', Page, metadata)
--- src/pages/2020/jul.js
+++ src/pages/2020/jul.js
@@ -5,6 +5,28 @@ import { Organization } from '../../models/organization.js'
 import { BanknoteInSimplexWind } from '../../scenes/banknote-in-simplex-wind.js'
 import { detectWebGL } from '../../models/webgl.js'
 
+const org = Organization.default
+export const canonicalUrl = `${org.url}/2020/jul.html`
+export const title = 'Jul 2020: A Banknote in Simplex Wind | Purvis Research'
+export const description = 'A Banknote In Simplex Wind'
+export const ogImageUrl = `${org.url}${ogImagePath}`
+export const ogTitle = 'Jul 2020'
+export const jsonld = JSON.stringify({
+  '@context': 'https://schema.org',
+  '@type': 'BreadcrumbList',
+  'itemListElement': [{
+    '@type': 'ListItem',
+    'position': 1,
+    'name': 'purvis research',
+    'item': org.url
+  },{
+    '@type': 'ListItem',
+    'position': 2,
+    'name': 'jul 2020',
+    'item': canonicalUrl
+  }]
+})
+
 export default {
   beforeDestroy() {
     this.dispose()
@@ -12,45 +34,24 @@ export default {
   components: {
     Graphix
   },
-  created() {
-    // Non-reactive data:
-    this.canonicalUrl = `${Organization.default.url}/2020/jul.html`
-    this.title = 'Jul 2020: A Banknote in Simplex Wind | Purvis Research'
-    this.description = 'A Banknote In Simplex Wind'
-    this.jsonld = {
-      '@context': 'https://schema.org',
-      '@type': 'BreadcrumbList',
-      'itemListElement': [{
-        '@type': 'ListItem',
-        'position': 1,
-        'name': 'purvis research',
-        'item': Organization.default.url
-      },{
-        '@type': 'ListItem',
-        'position': 2,
-        'name': 'jul 2020',
-        'item': this.canonicalUrl
-      }]
-    }
-  },
   head() {
     return {
-      title: this.title,
+      title,
       meta: [
-        { name: 'description', content: this.description, hid: 'description' },
-        { property:'og:description', content: this.description },
-        { property:'og:image', content: `${Organization.default.url}${ogImagePath}` },
+        { name: 'description', content: description, hid: 'description' },
+        { property:'og:description', content: description },
+        { property:'og:image', content: ogImageUrl },
         { property:'og:image:height', content:'859' },
         { property:'og:image:width', content:'1646' },
-        { property:'og:title', content:'Jul 2020' },
-        { property:'og:url', content: this.canonicalUrl },
+        { property:'og:title', content: ogTitle },
+        { property:'og:url', content: canonicalUrl },
         { name:'twitter:card', content:'summary_large_image' },
       ],
       link: [
-        { rel: 'canonical', href: this.canonicalUrl }
+        { rel: 'canonical', href: canonicalUrl }
       ],
       script: [
-        { type: 'application/ld+json', json: this.jsonld }
+        { type: 'application/ld+json', json: jsonld }
       ],
     }
   },
