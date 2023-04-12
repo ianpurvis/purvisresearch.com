d00c0950ef1ba1fc74f3686df5f6d162209f0e84
Ian Purvis <ian@purvisresearch.com>
2023-04-12 10:01:36 -0500

    Simplify babel config

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,58 +1,52 @@
 module.exports = (api) => {
   api.assertVersion('^7.0')
 
-  let presets
+  let presetAppOptions
   switch(api.env()) {
   case 'client':
-    presets = [
-      ['@nuxt/babel-preset-app-edge', {
-        corejs: 3,
-        targets: {
-          chrome: '41',
-          ie: '9'
-        }
-      }]
-    ]
+    presetAppOptions = {
+      corejs: 3,
+      targets: {
+        chrome: '41',
+        ie: '9'
+      }
+    }
     break
   case 'modern':
-    presets = [
-      ['@nuxt/babel-preset-app-edge', {
-        bugfixes: true,
-        corejs: 3,
-        targets: {
-          esmodules: true
-        }
-      }]
-    ]
+    presetAppOptions = {
+      bugfixes: true,
+      corejs: 3,
+      targets: {
+        esmodules: true
+      }
+    }
     break
   case 'server':
-    presets = [
-      ['@nuxt/babel-preset-app-edge', {
-        corejs: 3,
-        targets: {
-          node: 'current'
-        }
-      }]
-    ]
+    presetAppOptions = {
+      corejs: 3,
+      targets: {
+        node: 'current'
+      }
+    }
     break
   case 'test':
-    presets = [
-      ['@babel/preset-env', {
-        corejs: 3,
-        modules: 'commonjs',
-        targets: {
-          node: 'current'
-        },
-        useBuiltIns: 'usage'
-      }]
-    ]
+    presetAppOptions = {
+      corejs: 3,
+      modules: 'commonjs',
+      targets: {
+        node: 'current'
+      },
+      useBuiltIns: 'usage'
+    }
     break
   default:
     throw 'Unknown babel envName'
   }
 
   const config = {
-    presets,
+    presets: [
+      ['@nuxt/babel-preset-app', presetAppOptions]
+    ],
 
     // Polyfill CommonJS modules using 'require' syntax:
     sourceType: 'unambiguous',
