0d0f35577242dd79cf0efb6b890033c6e0474c09
Ian Purvis <ian@purvisresearch.com>
2023-04-12 11:31:20 -0500

    Add jest support for useHead from #imports

--- spec/setup.js
+++ spec/setup.js
@@ -2,3 +2,8 @@ import { expect } from 'jest-ctx'
 import * as matchers from './matchers.js'
 
 expect.extend(matchers)
+
+import { jest } from '@jest/globals'
+jest.mock('#imports', () => ({
+  useHead: jest.fn()
+}))
