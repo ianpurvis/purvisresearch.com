64e1a7f447b630b7fa2a84658d75cbe1f8fda65f
Ian Purvis <ian@purvisresearch.com>
2023-05-23 22:58:54 -0500

    Migrate babel config to @babel/preset-env

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,56 +1,13 @@
 module.exports = (api) => {
   api.assertVersion('^7.0')
+  api.cache(true)
 
-  let presetAppOptions
-  switch(api.env()) {
-  case 'client':
-    presetAppOptions = {
-      corejs: 3,
-      targets: {
-        chrome: '41',
-        ie: '9'
-      }
-    }
-    break
-  case 'modern':
-    presetAppOptions = {
-      bugfixes: true,
-      corejs: 3,
-      targets: {
-        esmodules: true
-      }
-    }
-    break
-  case 'server':
-    presetAppOptions = {
-      corejs: 3,
-      targets: {
-        node: 'current'
-      }
-    }
-    break
-  case 'test':
-    presetAppOptions = {
-      corejs: 3,
-      modules: 'commonjs',
-      targets: {
-        node: 'current'
-      },
-      useBuiltIns: 'usage'
-    }
-    break
-  default:
-    throw 'Unknown babel envName'
-  }
-
-  const config = {
+  return {
     presets: [
-      ['@nuxt/babel-preset-app', presetAppOptions]
+      '@babel/preset-env'
     ],
-
-    // Polyfill CommonJS modules using 'require' syntax:
-    sourceType: 'unambiguous',
+    targets: {
+      esmodules: true
+    }
   }
-
-  return config
 }
