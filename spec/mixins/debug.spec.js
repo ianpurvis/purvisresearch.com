40c9b5e3cfca5face67b30181bb826dbf18a842e
Ian Purvis <ian@purvisresearch.com>
2023-05-25 09:54:10 -0500

    chore: Migrate debug mixin to composition api (#922)

--- spec/mixins/debug.spec.js
+++ spec/mixins/debug.spec.js
@@ -3,71 +3,76 @@ jest.mock('~/assets/stylesheets/debug.scss', () => {}, { virtual: true })
 import { shallowMount } from '@vue/test-utils'
 import { jest } from '@jest/globals'
 import { beforeEach, describe, expect, it } from 'jest-ctx'
-import debug from '~/mixins/debug.js'
+import { useDebug } from '~/mixins/debug.js'
 
 
-describe('debug', () => {
+describe('useDebug()', () => {
   let component, wrapper
 
   beforeEach(() => {
     component = {
-      mixins: [
-        debug
-      ],
+      setup() {
+        useDebug()
+      },
       render: jest.fn()
     }
   })
-  describe('beforeDestroy()', () => {
-    it('removes handleKeyup from window keyup', () => {
-      global.window.removeEventListener = jest.fn()
-      wrapper = shallowMount(component, {
-        mounted: jest.fn()
-      })
-      wrapper.destroy()
-      expect(global.window.removeEventListener)
-        .toHaveBeenCalledWith('keyup', wrapper.vm.handleKeyup)
-    })
-  })
-  describe('mounted()', () => {
-    it('adds handleKeyup to window keyup', () => {
-      global.window.addEventListener = jest.fn()
+
+  describe('when mounted', () => {
+    let addEventListener
+
+    beforeEach(() => {
+      addEventListener = jest.spyOn(window, 'addEventListener')
       wrapper = shallowMount(component)
-      expect(global.window.addEventListener)
-        .toHaveBeenCalledWith('keyup', wrapper.vm.handleKeyup)
     })
-  })
-  describe('methods', () => {
-    describe('handleKeyup(event)', () => {
-      let event
+
+    it('listens for window keyup events', () => {
+      expect(addEventListener).toHaveBeenCalledWith('keyup', expect.any(Function))
+    })
+
+    describe('when a window keyup event is heard', () => {
+      let event, toggleAttribute
 
       beforeEach(() => {
-        component.methods = {
-          toggleDebugMode: jest.fn()
-        }
-        wrapper = shallowMount(component)
+        toggleAttribute = jest.spyOn(document.documentElement, 'toggleAttribute')
       })
+
       describe('when event prevents default', () => {
         it('does nothing', () => {
-          event = { defaultPrevented: true }
-          wrapper.vm.handleKeyup(event)
-          expect(component.methods.toggleDebugMode).not.toHaveBeenCalled()
+          event = new KeyboardEvent('keyup')
+          event.preventDefault()
+          window.dispatchEvent(event)
+          expect(toggleAttribute).not.toHaveBeenCalled()
         })
       })
-      describe('when event key is \'d\'', () => {
+
+      describe('when event key is not "d"', () => {
+        it('does nothing', () => {
+          event = new KeyboardEvent('keyup', { key: 'x' })
+          window.dispatchEvent(event)
+          expect(toggleAttribute).not.toHaveBeenCalled()
+        })
+      })
+
+      describe('when event key is "d"', () => {
         it('toggles debug mode', () => {
-          event = { defaultPrevented: false, key: 'd' }
-          wrapper.vm.handleKeyup(event)
-          expect(component.methods.toggleDebugMode).toHaveBeenCalled()
+          event = new KeyboardEvent('keyup', { key: 'd' })
+          window.dispatchEvent(event)
+          expect(toggleAttribute).toHaveBeenCalledWith('debug')
         })
       })
     })
-    describe('toggleDebugMode()', () => {
-      it('toggles the debug attribute on the window document element', () => {
-        global.document.documentElement.toggleAttribute = jest.fn()
-        wrapper = shallowMount(component)
-        wrapper.vm.toggleDebugMode()
-        expect(global.document.documentElement.toggleAttribute)
-          .toHaveBeenCalledWith('debug')
+
+    describe('when unmounted', () => {
+      let removeEventListener
+
+      beforeEach(() => {
+        removeEventListener = jest.spyOn(window, 'removeEventListener')
+        wrapper.destroy()
+      })
+
+      it('stops listening for window keyup events', () => {
+        expect(removeEventListener).toHaveBeenCalledWith('keyup', expect.any(Function))
       })
     })
   })
