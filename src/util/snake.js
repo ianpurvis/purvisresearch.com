7e2b5d1d1c6bd93440c9f4066b431409af252602
Ian Purvis <ian@purvisresearch.com>
2024-01-11 15:44:02 -0600

    Add full width underscore support to snake util

--- src/util/snake.js
+++ src/util/snake.js
@@ -2,5 +2,7 @@ import { getCurrentInstance } from 'vue'
 
 export function snake(value) {
   const { proxy: { $options: { __scopeId } } } = getCurrentInstance()
-  return value.replace(/\s/g, `<span class="underscore" ${__scopeId}>&nbsp;</span>`)
+  return value.replace(/\s/g, (match) => {
+    return `<span class="underscore" ${__scopeId}>${match}</span>`
+  })
 }
